a20f69cc41e6df497cccb1af95497c1b17834781
gyeongdeang <joohyeon0286@gmail.com>
2022-08-09 11:03:38 +0900

    first

---
+++ src/main/java/com/codestates/practicedeploy/PracticeDeployApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.practicedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDeployApplication.class, args);
+	}
+
+}
